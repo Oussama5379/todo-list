f1c98b71881a888d27fe08313d1a5b5f935b1b3d
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-03-30 13:25:34 +0100

    Change manipulating projects based on the new definition of objects

--- src/DomManipulation.js
+++ src/DomManipulation.js
@@ -4,12 +4,15 @@ import {
 	deleteProjectFromStorage,
 	allStorage,
 	clearStorage,
+	getProjectFromStorage,
 } from "./storage.js";
 const newTodoBtn = document.querySelector(".new-todo-btn");
 const todoDialog = document.querySelector(".todo-dialog");
 const newProjectBtn = document.querySelector(".new-project-btn");
 const projectDialog = document.querySelector(".project-dialog");
 const projectSubmit = document.querySelector(".project-submit");
+const todoSubmit = document.querySelector(".todo-submit");
+
 newTodoBtn.addEventListener("click", () => {
 	todoDialog.showModal();
 });
@@ -18,18 +21,21 @@ newProjectBtn.addEventListener("click", () => {
 });
 projectSubmit.addEventListener("click", (event) => {
 	event.preventDefault();
+	//Add the project to the local storage
 	const projectName = document.querySelector("#project-name").value;
 	let project = Project(projectName);
-	let alternativeProject = {
-		id: project.getID(),
-		name: project.getName(),
-		todoList: project.getTodoList(),
-	};
-	addProjectToStorage(project.getID(), JSON.stringify(alternativeProject));
+
+	addProjectToStorage(project.id, JSON.stringify(project));
 	//Add the project to the page
 	const projectsList = document.querySelector(".projects-list");
 	const projectButton = document.createElement("button");
 	projectButton.textContent = projectName;
 	projectButton.id = project.id;
 	projectsList.appendChild(projectButton);
+	//Add the project to the projects list in the todo form
+	const projectSelect = document.querySelector("#todo-project");
+	const option = document.createElement("option");
+	option.value = project.id;
+	option.textContent = projectName;
+	projectSelect.appendChild(option);
 });
